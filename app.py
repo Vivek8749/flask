5012393c2612325887ed1a2b70194be2a00ece39
Vivek8749 <vivek.maurya9087@gmail.com>
2024-12-30 18:26:37 +0530

    fixed

--- app.py
+++ app.py
@@ -11,4 +11,4 @@ def hello_name(name):
     return f"Hello {name} how are you?"
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0:5000')
+    app.run(debug=True, host='0.0.0.0', port=5000)
